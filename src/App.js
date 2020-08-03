f09b9a90264777376e31426f896f0a8719b0f891
Ghaze07 <68465222+Ghaze07@users.noreply.github.com>
2020-08-03 08:41:33 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+function App({name, age}) {
+
+  return  <div>
+    Hello from App.js updated {name} Age = {age - 5}
+    <br />
+    <Hello firstName={name}></Hello>
+    </div>
+    
+
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hello.css'
+
+
+function Hello({firstName}) {
+
+return <p className="myname">This is Para in Hello tag : {firstName}</p> 
+    
+
+}
+
+export default Hello;
